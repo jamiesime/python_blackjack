6f39034431bb8731cc6afa32ace4955080d9b55a
GHOST-HUB\Jamie <jamiesime@gmail.com>
2018-02-14 16:02:20 +0000

    tested player starts with no cards

--- player_test.py
+++ player_test.py
@@ -4,13 +4,13 @@ from player import Player
 class PlayerTests(unittest.TestCase):
 
 	def setUp(self):
-		self.player = Player("Bramble", "none")
+		self.player = Player("Bramble", [])
 
 	def testName(self):
 		self.assertTrue(self.player.getName() == "Bramble")
 
 	def testCards(self):
-		self.assertTrue(self.player.getCards() == "none")
+		self.assertTrue(len(self.player.getCards()) == 0)
 
 
 
